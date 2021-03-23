61b0b9831fb79c9116b0605ae8dbb51edea7e6ae
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-03-23 14:08:10 +0000

    Check comments start with '! '

--- tests/test_reference.f90
+++ tests/test_reference.f90
@@ -35,5 +35,6 @@ do i = 1, 10, n
 
 x = 1 + 2 * 3 / 5
 
+! "var=0"  ! First spaces should be squashed into one
 ! "var=1"  ! Shouldn't be changed
-! "var=2"  !only first space have to be added
+! "var=2"  ! Only first space have to be added
