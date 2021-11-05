86450c9c837e8007d1aa7ac91fe6792f4d672a3a
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-11-05 20:31:15 +0000

    Migrate to setup.cfg

--- setup.py
+++ setup.py
@@ -1,37 +1,3 @@
-import os
-
 from setuptools import setup
 
-package_name = "fortran_linter"
-version = "1.0.9"
-README = os.path.join(os.path.dirname(__file__), "Readme.md")
-long_description = open(README).read()
-setup(
-    name=package_name,
-    version=version,
-    description=("A linter for Fortran files"),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cphyc/fortran-linter",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Fortran",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",  # noqa: E501
-        "Topic :: Software Development :: Build Tools",
-    ],
-    keywords="fortran",
-    author="Corentin Cadiou",
-    author_email="corentin.cadiou@iap.fr",
-    license="GPL",
-    package_dir={package_name: package_name},
-    packages=[package_name],
-    package_data={package_name: ["Readme.md", "LICENSE"]},
-    entry_points={
-        "console_scripts": [
-            "fortran-linter = fortran_linter.cli:main",
-        ]
-    },
-    include_package_data=True,
-    setup_requires=(["wheel"]),
-)
+setup()
